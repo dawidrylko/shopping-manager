4df79889f02be9b0d621d659dcc972c1631de3c4
Dawid Ryłko <dawid.rylko@gmail.com>
2017-03-05 18:18:22 +0100

    Create rest handler getProducts

--- api/main.go
+++ api/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("shopping-manager api start")
+}
+
+type Product struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+type Products []Product
+
+func getProducts(responseWriter http.ResponseWriter, request *http.Request) {
+	products := Products{
+		Product{ID: 1, Name: "ogórek zielony"},
+		Product{ID: 2, Name: "papryka czerwona"},
+		Product{ID: 3, Name: "Sałata lodowa"},
+	}
+
+	json.NewEncoder(responseWriter).Encode(products)
 }
