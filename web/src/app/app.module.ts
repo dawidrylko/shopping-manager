e98e8fb1ba39f74abc9b68396ecedb48beb8e1a0
Dawid Ryłko <dawid.rylko@gmail.com>
2017-03-16 19:53:09 +0100

    Create product route

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -6,6 +6,19 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
 
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { 
+    path: 'product', 
+    component: ProductComponent 
+  },
+  { 
+    path: '**', 
+    component: PageNotFoundComponent 
+  }
+];
+
 @NgModule({
   declarations: [
     AppComponent,
