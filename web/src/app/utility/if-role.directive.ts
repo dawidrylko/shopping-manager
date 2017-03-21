53f5dab1cb399753f03d250a23ec3dd8e1d77e81
Dawid Ryłko <dawid.rylko@gmail.com>
2017-03-21 21:41:22 +0100

    Create updateViewVisibility method

--- web/src/app/utility/if-role.directive.ts
+++ web/src/app/utility/if-role.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, OnInit, Input } from '@angular/core';
+import { Directive, OnInit, Input, ViewContainerRef, TemplateRef } from '@angular/core';
 
 @Directive({
   selector: '[ifRole]'
@@ -8,9 +8,26 @@ export class IfRoleDirective implements OnInit {
   @Input('ifRole')
   public roles: string[];
 
-  constructor() { }
+  private hasView: boolean = false;
+
+  constructor(
+    private viewContainerRef: ViewContainerRef,
+    private templateRef: TemplateRef<any>
+  ) { }
 
   ngOnInit() {
+  }
+
+  private updateViewVisibility(): void {
+    let isRole = !!this.roles.length;
+
+    if (isRole) {
+      this.viewContainerRef.createEmbeddedView(this.templateRef);
+    } else {
+      this.viewContainerRef.clear();
+    }
+
+    this.hasView = isRole;
   }
 
 }
